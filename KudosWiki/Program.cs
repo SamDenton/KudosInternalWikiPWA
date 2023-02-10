ae18299ce7f80f83bfe912452f7b986c876b5660
Sam Denton <sam.denton@kudos-software.co.uk>
2023-02-10 17:32:38 +0000

    Finally got route parametrization working so content is displayed only when relevant.  Just need to get navMenu to update when new content section is added to show the new section.

--- KudosWiki/Program.cs
+++ KudosWiki/Program.cs
@@ -1,13 +1,16 @@
 using KudosWiki;
+using KudosWiki.Shared;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using static KudosWiki.Pages.MainContent;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+builder.Services.AddSingleton<ContentHolderState>();
+builder.Services.AddSingleton<contentHolder>();
 builder.Services.AddMsalAuthentication(options =>
 {
     builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
