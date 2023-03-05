d3c73be10a43cdb7e6fa1f785b084b256fdfcdb3
SamDentonHome <samw.denton@gmail.co.uk>
2023-03-05 01:07:45 +0000

    Added JS to animate navbar, improved layout a lot

---
+++ KudosWiki/wwwroot/navbar.js
@@ -0,0 +1,51 @@
+﻿function initNavbar() {
+    var navbar = document.querySelector('.sidebarController');
+    var icon = document.querySelector('.icon');
+    var mq = window.matchMedia('(max-width: 689.98px)');
+
+    // Set initial state based on screen size
+    if (mq.matches) {
+        navbar.classList.add('navHider');
+        icon.classList.remove('rotateLeft');
+        icon.classList.add('rotateRight');
+    } else {
+        navbar.classList.add('navShower');
+        icon.classList.remove('rotateRight');
+        icon.classList.add('rotateLeft');
+    }
+
+    // Listen for clicks on the icon
+    icon.addEventListener('click', function () {
+        if (navbar.classList.contains('navShower')) {
+            navbar.classList.remove('navShower');
+            navbar.classList.add('navHider');
+            icon.classList.remove('rotateLeft');
+            icon.classList.add('rotateRight');
+        } else {
+            navbar.classList.remove('navHider');
+            navbar.classList.add('navShower');
+            icon.classList.remove('rotateRight');
+            icon.classList.add('rotateLeft');
+        }
+    });
+
+    // Listen for screen size changes
+    mq.addListener(function () {
+        if (mq.matches) {
+            navbar.classList.remove('navShower');
+            navbar.classList.add('navHider');
+            icon.classList.remove('rotateLeft');
+            icon.classList.add('rotateRight');
+        } else {
+            navbar.classList.remove('navHider');
+            navbar.classList.add('navShower');
+            icon.classList.remove('rotateRight');
+            icon.classList.add('rotateLeft');
+        }
+    });
+}
+icon.classList.remove('rotateRight');
+icon.classList.add('rotateLeft');
+
+icon.classList.remove('rotateLeft');
+icon.classList.add('rotateRight');
