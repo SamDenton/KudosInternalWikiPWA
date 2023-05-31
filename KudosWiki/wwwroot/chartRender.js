e5081edf0c043a978a80569447e93b40c552f11a
Sam Denton <samw.denton@gmail.com>
2023-05-31 18:45:59 +0100

    added the start of a graphing/reporting tool that should use AI to generate JSON objects that can be rendered by chart.js

---
+++ KudosWiki/wwwroot/chartRender.js
@@ -0,0 +1,23 @@
+var myChart;
+
+window.renderGraph = function (jsonData) {
+    var canvas = document.getElementById('chartContainer');
+
+    var data = JSON.parse(jsonData);
+
+    if (myChart) {
+        myChart.destroy();
+    }
+
+    myChart = new Chart(canvas, {
+        type: data.type,
+        data: {
+            labels: data.labels,
+            datasets: data.datasets
+        },
+        options: data.options
+    });
+};
+    /*var ctx = canvas.getContext('2d');*/
+
+    /*new Chart(canvas, JSON.parse(graphData));*/
